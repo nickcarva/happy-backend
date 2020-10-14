7099b6aa0e81b62bf883e0e2dd4d07abb346975b
nickcarva <carvalhonick2002@gmail.com>
2020-10-13 22:38:47 -0300

    Migrations + Validations implemented

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
 import express from 'express'
+import path from 'path'
+import cors from 'cors'
+
+import './database/connection'
+
+import errorHandler from './errors/handler'
+import routes from './routes'
 
 const app = express()
 
+app.use(cors)
 app.use(express.json())
-
-app.post('/users/:id', (req, res) => {
-  console.log(req.query)
-  console.log(req.params.id)
-  console.log(req.body)
-
-  return res.json({ message: 'Hello World' })
-})
+app.use(routes)
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler)
 
 app.listen(3333)
